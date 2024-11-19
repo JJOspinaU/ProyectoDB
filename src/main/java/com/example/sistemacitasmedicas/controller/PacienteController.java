c956ba5294003ce7ba335a99c102216c110aa8cc
JJOspinaU <juan_josn.ospina@uao.edu.co>
2024-11-19 13:26:19 -0500

    Add files via upload

--- src/main/java/com/example/sistemacitasmedicas/controller/PacienteController.java
+++ src/main/java/com/example/sistemacitasmedicas/controller/PacienteController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 import java.util.List;
 
 @RestController
@@ -42,10 +44,45 @@ public class PacienteController {
         return ResponseEntity.ok(pacientes);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Paciente> updatePaciente(@PathVariable Integer id, @RequestBody Paciente paciente) {
+        Optional<Paciente> pacienteOptional = pacienteService.findById(id);
+
+        if (pacienteOptional.isPresent()) {
+            Paciente pacienteExistente = pacienteOptional.get();
+
+            // Actualiza los datos del paciente existente
+            pacienteExistente.setNombre(paciente.getNombre());
+            pacienteExistente.setFechaNacimiento(paciente.getFechaNacimiento());
+            pacienteExistente.setAlergias(paciente.getAlergias());
+            pacienteExistente.setCondicionesClinicas(paciente.getCondicionesClinicas());
+            pacienteExistente.setPrioridadAtencion(paciente.getPrioridadAtencion());
+            pacienteExistente.setEps(paciente.getEps());
+
+            Paciente pacienteActualizado = pacienteService.save(pacienteExistente);
+            return ResponseEntity.ok(pacienteActualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+public ResponseEntity<Void> deletePaciente(@PathVariable Integer id) {
+    Optional<Paciente> pacienteOptional = pacienteService.findById(id);
+
+    if (pacienteOptional.isPresent()) {
+        pacienteService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+}
+
     @GetMapping("/{id}")
     public ResponseEntity<Paciente> getPacienteById(@PathVariable Integer id) {
     return pacienteService.findById(id)
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.notFound().build());
 }
+    
 }
